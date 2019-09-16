ab453ffaa33c55dc2bfb084334b719df0f49db13
TTsmall <914158857@qq.com>
2019-09-16 12:32:37 +0800

    Update main.go

--- main.go
+++ main.go
@@ -22,21 +22,21 @@ func (this *addRepChan) Execute() {
 func main() {
 	var routines int = 10
 	var items int = 100000
-	fmt.Println("1 time=%s", time.Now().Format("2006-01-02 15:04:05.000"))
+	fmt.Println("1 time=", time.Now().Format("2006-01-02 15:04:05.000"))
 	workPools := gopool.NewPoolMultiParam(routines, items)
 	for count := 0; count < 1000000; count++ {
 		workPools.Add(&addRepChan{cmd: count})
 	}
 	workPools.Stop()
-	fmt.Println("2 time=%s", time.Now().Format("2006-01-02 15:04:05.000"))
+	fmt.Println("2 time=", time.Now().Format("2006-01-02 15:04:05.000"))
 	for count := 0; count < 1000000; count++ {
 		addChan := &addRepChan{cmd: count}
 		addChan.Execute()
 	}
-	fmt.Println("3 time=%s", time.Now().Format("2006-01-02 15:04:05.000"))
+	fmt.Println("3 time=", time.Now().Format("2006-01-02 15:04:05.000"))
 }
 
 
-1 time=%s 2019-09-16 12:29:19.825
-2 time=%s 2019-09-16 12:29:24.970
-3 time=%s 2019-09-16 12:29:44.603
+1 time= 2019-09-16 12:29:19.825
+2 time= 2019-09-16 12:29:24.970
+3 time= 2019-09-16 12:29:44.603
